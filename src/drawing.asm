2e37bd4b0a5b2ce693415023117eb0138dd84733
Mateusz Olczyk <matik60@gmail.com>
2021-02-05 16:23:11 +0100

    Translated comments from drawing.asm

--- src/drawing.asm
+++ src/drawing.asm
@@ -1,103 +1,110 @@
-; Wybiera pozycjê startow¹ rysowania na podstawie liczby znaków w CX
-; ARG: cl = liczba znaków w buforze
-; RET: es:[di] = pozycja startowa
+; Finds the position of the first pixel of the barcode according to the number of bytes to be
+; encoded. This calculation is needed to center the barcode horizontally and vertically. Every pixel
+; till the starting position will be painted white.
+; [in] CL - number of bytes to be encoded
+; [out] ES:[DI] - starting position
 PROC_startpoint:
 push cx
-	mov di, 0a000h								; ustawienie ES:[DI] na pierwszy piksel
-	mov es, di
-	mov di, 0
-	
-	mov al, 11									; OBLICZANIE POZYCJI STARTOWEJ
-	mul cl
-	add ax, 35
-	shr ax, 1
-	neg ax
-	add ax, 25760
-	mov cx, ax									; pozycja startowa w CX
-	
-	mov al, CONST_color_white					; zamalowanie pocz¹tku na bia³o
-	rep stosb
+    mov di, 0a000h              ; set ES:[DI] to the address of the first pixel of VGA memory
+    mov es, di
+    mov di, 0
+    
+    mov al, 11
+    mul cl
+    add ax, 35
+    shr ax, 1
+    neg ax
+    add ax, 25760
+    mov cx, ax                  ; starting postion offset is now in CX
+    
+    mov al, CONST_color_white   ; paint everything before the starting position to white
+    rep stosb
 pop cx
 ret
-;***************************************************************************************************
-; Wyœwietla string pod adresem AX
-; ARG: al = znak do przetworzenia (wg tabeli znaków CODE-128)
-;      ds = SEG_data
-;      es:[di] = pocz¹tek rysowania
-; RET: ax = kod znaku w notacji pikselowej, di += 11
+
+
+; Draws a single barcode element at index AX in array VAR_codetable.
+; [in] AL - index of VAR_codetable
+; [in] DS - expected to be SEG_data
+; [in] ES:[DI] - starting position of drawing
+; [out] AX - barcode corresponding to passed VAR_codetable index
+; [out] DI - increased by 11 which corresponds to single barcode length in pixels
 PROC_drawcode:
 push bx
 push cx
 push dx
-											; ZAMIANA WARTOŒCI ZNAKU Z AL NA KOD PIKSELOWY W AX
-	shl al, 1								; wyznaczanie adresu kodu w BX
-	mov bh, 0
-	mov bl, al
-	add bx, offset VAR_codetable
-	mov ax, word ptr ds:[bx]				; wczytanie do AX kodu pikselowego odpowiadaj¹cego danemu znakowi
+    shl al, 1                       ; multiply address by 2 because VAR_codetable is array of words
+    mov bh, 0                       ; and a word has 2 bytes size
+    mov bl, al
+    add bx, offset VAR_codetable    ; set BX to address of corresponding barcode value
+    mov ax, word ptr ds:[bx]        ; set AX to the value at VAR_codetable[AL]
 
-	mov dx, ax									; zapamiêtaj ci¹g pikseli w DX
-	mov cx, 11
-	mov bx, 10000000000b
-	PROC_drawcode_pixel:
-		and ax, bx								; testuj bit
-		jnz PROC_drawcode_black
-		mov al, CONST_color_white
-		jmp PROC_drawcode_continue
-		PROC_drawcode_black:
-		mov al, CONST_color_black
-		PROC_drawcode_continue:
-		stosb
-		shr bx, 1								; testuj kolejny bit
-		mov ax, dx								; wczytaj ci¹g z DX
-	loop PROC_drawcode_pixel
+    mov dx, ax                      ; save AX in DX before it will be detroyed
+    mov cx, 11
+    mov bx, 10000000000b
+    PROC_drawcode_pixel:
+        and ax, bx                  ; test bit and paint a pixel white or black
+        jnz PROC_drawcode_black
+        mov al, CONST_color_white
+        jmp PROC_drawcode_continue
+        PROC_drawcode_black:
+        mov al, CONST_color_black
+        PROC_drawcode_continue:
+        stosb
+        shr bx, 1                   ; test next bit
+        mov ax, dx                  ; set AX to its original state
+    loop PROC_drawcode_pixel
 pop dx
 pop cx
 pop bx
 ret
-;***************************************************************************************************
-; Akumuluje sumê kontroln¹
-; ARG: al = char code,
-;      bl = bie¿¹ca pozycja w kodzie,
-;	   dl = bie¿¹ca wartoœæ sumy kontrolnej
-; RET: bl++, dl = nowa wartoœæ sumy kontrolnej
+
+
+; Calculates checksum value
+; [in] AL - index of VAR_codetable
+; [in] BL - current position in barcode
+; [in] DL - current value of the checksum
+; [out] BL - incremented
+; [out] DL - new value of the checksum
 PROC_checksum:
 push ax
-	mul bl										; ax = al*bl = sk³adnik sumy
-	mov dh, 0
-	add ax, dx									; nowa wartoœæ w ax
-	push bx
-	mov bl, 103									; wartoœæ modulo sumy kontrolnej
-	div bl										; ah = wartoœæ sumy kontrolnej modulo 103
-	pop bx										; przywróæ do bx wartoœæ pozycji
-	inc bl										; nastêpna pozycja
-	mov dl, ah									; aktualizacja dl
+    mul bl
+    mov dh, 0
+    add ax, dx
+    push bx
+    mov bl, 103
+    div bl
+    pop bx
+    inc bl
+    mov dl, ah
 pop ax
 ret
-;***************************************************************************************************
-; Wyœwietla znak stopu
-; ARG: es:[di] = pocz¹tek rysowania
-; RET: ax = kod znaku stopu w notacji pikselowej, di += 13
+
+
+; Draws the stop character of the barcode
+; [in] ES:[DI] - starting position of drawing
+; [out] AX - stop character barcode
+; [out] DI - increased by 13 which corresponds to stop character length in pixels
 PROC_drawcodestop:
 push bx
 push cx
 push dx
-	mov ax, 1100011101011b						; wczytanie do AX kodu pikselowego znaku stopu
-	mov dx, ax									; zapamiêtaj ci¹g pikseli w DX
-	mov cx, 13
-	mov bx, 1000000000000b
-	PROC_drawcodestop_pixel:
-		and ax, bx								; testuj bit
-		jnz PROC_drawcodestop_black
-		mov al, CONST_color_white
-		jmp PROC_drawcodestop_continue
-		PROC_drawcodestop_black:
-		mov al, CONST_color_black
-		PROC_drawcodestop_continue:
-		stosb
-		shr bx, 1								; testuj kolejny bit
-		mov ax, dx								; wczytaj ci¹g z DX
-	loop PROC_drawcodestop_pixel
+    mov ax, 1100011101011b              ; set AX to the stop code
+    mov dx, ax                          ; save AX in DX before it will be detroyed
+    mov cx, 13
+    mov bx, 1000000000000b
+    PROC_drawcodestop_pixel:
+        and ax, bx                      ; test bit and paint a pixel white or black
+        jnz PROC_drawcodestop_black
+        mov al, CONST_color_white
+        jmp PROC_drawcodestop_continue
+        PROC_drawcodestop_black:
+        mov al, CONST_color_black
+        PROC_drawcodestop_continue:
+        stosb
+        shr bx, 1                       ; test next bit
+        mov ax, dx                      ; set AX to its original state
+    loop PROC_drawcodestop_pixel
 pop dx
 pop cx
 pop bx
