868f37b80c901cb6d2612d8879e17e3a8d613c72
Mateusz Olczyk <matik60@gmail.com>
2021-02-05 12:56:22 +0100

    Translated comments from printstr.asm

--- src/printstr.asm
+++ src/printstr.asm
@@ -1,15 +1,14 @@
-; Wyœwietla string pod adresem AX
-; ARG: ax = offset stringa zakoñczonego znakiem '$'
-; RET: ---
+; Print a string under address passed in AX
+; [in] AX - address of the string variable. The string must end with '$' character.
 PROC_printstr:
 push ax
 push dx
 push ds
-	mov dx, seg SEG_data
-	mov ds, dx
-	mov dx, ax
-	mov ah, 9h
-	int 21h
+    mov dx, seg SEG_data
+    mov ds, dx
+    mov dx, ax
+    mov ah, 9h
+    int 21h
 pop ds
 pop dx
 pop ax	
